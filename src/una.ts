20cabb8a55d9147d62d035de38c12b6b2cc53a4e
Dolu <doludev@protonmail.com>
2021-10-27 21:04:43 +0200

    Refacto backend init

--- src/una.ts
+++ src/una.ts
@@ -1,10 +1,9 @@
-import { EclairRest, LndRest } from './backends'
+import { EclairRest, IBackend, LndRest } from './backends'
 import { EBackendType } from './enums'
 import { IEclairRest, ILndRest, Invoice } from './interfaces'
 
 export class Una {
-  private readonly lndRest: LndRest | undefined
-  private readonly eclairRest: EclairRest | undefined
+  private readonly client: IBackend | undefined
 
   constructor (backend: EBackendType, connectionInformation: ConnectionInformation) {
     if (!this.verifyConnectionInformation(connectionInformation, backend)) {
@@ -13,10 +12,10 @@ export class Una {
 
     if (backend === EBackendType.LndRest) {
       const info = connectionInformation as ILndRest
-      this.lndRest = new LndRest({ url: info.url, hexMacaroon: info.hexMacaroon })
+      this.client = new LndRest({ url: info.url, hexMacaroon: info.hexMacaroon })
     } else if (backend === EBackendType.EclairRest) {
       const info = connectionInformation as IEclairRest
-      this.eclairRest = new EclairRest({ url: info.url, user: info.user, password: info.password })
+      this.client = new EclairRest({ url: info.url, user: info.user, password: info.password })
     } else {
       throw new Error('Backend not supported.')
     }
@@ -29,13 +28,11 @@ export class Una {
        * @returns Invoice
        */
   public async createInvoice (amount: number, memo: string): Promise<Invoice> {
-    if (this.lndRest !== undefined) {
-      return await this.lndRest.createInvoice(amount, memo)
-    } else if (this.eclairRest !== undefined) {
-      return await this.eclairRest.createInvoice(amount, memo)
-    } else {
+    if (this.client === undefined) {
       throw new Error('No backend defined')
     }
+
+    return await this.client.createInvoice(amount, memo)
   }
 
   /**
@@ -44,13 +41,11 @@ export class Una {
        * @returns Invoice
        */
   public async getInvoice (hash: string): Promise<Invoice> {
-    if (this.lndRest !== undefined) {
-      return await this.lndRest.getInvoice(hash)
-    } else if (this.eclairRest !== undefined) {
-      return await this.eclairRest.getInvoice(hash)
-    } else {
+    if (this.client === undefined) {
       throw new Error('No backend defined')
     }
+
+    return await this.client.getInvoice(hash)
   }
 
   private verifyConnectionInformation (connectionInformation: ConnectionInformation, backend: EBackendType): boolean {
