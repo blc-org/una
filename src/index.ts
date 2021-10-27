958f68165310b4de46c2dc250cecdffdf26e8a66
Dolu <doludev@protonmail.com>
2021-10-27 19:14:22 +0200

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { Una } from './una.js'
+export { Una } from './una.js'
 export { IEclairRest, ILndRest, Invoice } from './interfaces'
 export { EBackendType } from './enums'
-export default Una
