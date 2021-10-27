39abe227b7e719498240eacfe0095cfff76933da
Dolu <doludev@protonmail.com>
2021-10-27 18:54:53 +0200

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { Una } from './una.js'
 export { IEclairRest, ILndRest, Invoice } from './interfaces'
-export { EBackendType, EHttpVerb } from './enums'
+export { EBackendType } from './enums'
 export default Una
