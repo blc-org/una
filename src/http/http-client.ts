69b8b214f293afdbaf943e0d1cc3dba416d68443
Lounès Ksouri <dev@louneskmt.com>
2021-10-28 23:18:56 +0200

    refactor: builtin http client

---
+++ src/http/http-client.ts
@@ -0,0 +1,38 @@
+import * as http from 'http'
+import * as https from 'https'
+import { EHttpVerb } from '../enums'
+
+export const request = async (url: string, options: https.RequestOptions, data: unknown = null): Promise<any> => {
+  const httpClient = url.includes('https://') ? https : http
+
+  return await new Promise((resolve, reject) => {
+    const req = httpClient.request(url, options, (res: any) => {
+      res.setEncoding('utf8')
+
+      let responseBody = ''
+      res.on('data', (chunk: string) => {
+        responseBody += chunk
+      })
+
+      res.on('end', () => {
+        const parsedBody = JSON.parse(responseBody)
+
+        if (parsedBody.error) {
+          return reject(new Error(parsedBody.error))
+        }
+
+        resolve(parsedBody)
+      })
+    })
+
+    req.on('error', (err: unknown) => {
+      reject(err)
+    })
+
+    if (options.method !== EHttpVerb.GET) {
+      req.write(data)
+    }
+
+    req.end()
+  })
+}
