c74c6013c8d95cc6e7002fd43a87d1760d31f399
Lounès Ksouri <dev@louneskmt.com>
2021-10-31 13:12:00 +0100

    refactor: use abstract class for clnbase rpc

--- src/backends/cln-socket/cln-socket.ts
+++ src/backends/cln-socket/cln-socket.ts
@@ -1,13 +1,17 @@
+import * as net from 'net'
 import { rpcRequest } from '../../rpc'
 import { ClnBase } from '..'
 import { IClnSocketUnix, IClnSocketTcp } from '../../interfaces'
 
 export default class ClnSocket extends ClnBase {
+  protected readonly clnConfig: IClnSocketUnix | IClnSocketTcp
+
   constructor (clnSocket: IClnSocketUnix | IClnSocketTcp) {
-    super(clnSocket)
+    super()
+    this.clnConfig = clnSocket
   }
 
-  public async request (config: any, body: any): Promise<any> {
+  protected async request (config: net.NetConnectOpts, body: any): Promise<any> {
     return await rpcRequest(config, body)
   }
 }
